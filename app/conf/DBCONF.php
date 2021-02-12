19b09c113ae9cb1635bf6fb6f440bf81c5260a0a
Jeroen <jf.rijkse@student.avans.nl>
2021-02-12 23:51:07 +0100

    First commit: db-connection working

---
+++ app/conf/DBCONF.php
@@ -0,0 +1,15 @@
+<?php
+
+//database config file
+
+//rename this file to DBCONF.php, and enter database credentials
+//IMPORTANT: verify DBCONF.php is in .gitignore
+class DBCONF{
+    const HOSTNAME = "localhost";
+    const DBNAME = 'admin_maskifysignin';
+    const USER = "admin_maskifysignin";
+    const PASSWORD = "maskifysignin";
+    // NAMESPACE should be a valid UUID. You can use the default one, or
+    // generate one here: https://www.uuidgenerator.net/
+    const NAMESPACE = "c416205f-49fa-4e90-91f7-e39a1fa0c4c0";
+}
