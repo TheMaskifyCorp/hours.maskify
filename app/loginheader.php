9ceff32f5fe5db1a98255f0ac2a003409a0ce5fe
gemma1987 <77026023+gemma1987@users.noreply.github.com>
2021-05-22 17:15:22 +0200

    Add files via upload

---
+++ app/loginheader.php
@@ -0,0 +1,24 @@
+<head>
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="..\CSS\Stylesheet.css">
+</head>
+<body>        
+    <div class="container-fluid header">
+            <div class="row">
+                <div class="col-md-8">
+                    <a href="home.php">
+                        <img src="..\Images\Logo_maskify.svg" alt="Logo maskify" width="250" height="250">
+                    </a>
+                </div>
+                <div class="col-md-4 texttop">
+                        <a href="faq.php">FAQ</a>
+                </div>
+            </div>
+        </div>
+</body>
