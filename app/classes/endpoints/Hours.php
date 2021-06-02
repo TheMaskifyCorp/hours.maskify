8bfc8795f69e4a25ce2ea2c61ffbcbedf84f5af1
Jeroen Rijkse <jeroen@rijkse>
2021-06-02 19:56:57 +0200

    finished get, started put

--- app/classes/endpoints/Hours.php
+++ app/classes/endpoints/Hours.php
@@ -19,30 +19,57 @@ class Hours implements ApiEndpointInterface
 
     public function get(array $body, array $params): array
     {
-        //TODO check manager and employee
+        //check manager and employee for authorisation
         if ( ( (! isset($params['itemid']) ) OR ( $this->employee != $params [ 'itemid' ] ) ) AND ( !$this->manager) ) throw new NotAuthorizedException('Hours can only be viewed by a manager or the object employee');
         //throw error for filtering on department AND employee
         if((isset($params['itemid'])) AND (isset($params['departmentid']))) throw new BadRequestException("Cannot filter on both single Employee and Department");
 
         //add every parameter to an array
+        $selection =[
+            "EmployeeHoursID",
+            "employeehours.EmployeeID",
+            "HoursAccorded",
+            "AccordedByManager",
+            "DeclaratedDate",
+            "EmployeeHoursQuantityInMinutes"];
         $where = [];
         if(isset($params['departmentid'])) array_push($where,["DepartmentID",'=',$params['departmentid']]);
-        if(isset($params['startdate'])) array_push($where,["DeclaratedDate",'>=',$params['startdate']]);
-        if(isset($params['enddate'])) array_push($where,["DeclaratedDate",'<=',$params['enddate']]);
-        if(isset($params['itemid'])) array_push($where,["employeehours.EmployeeID",'=',$params['itemid']]);
+        if(isset($params['startdaterange'])) array_push($where,["DeclaratedDate",'>=',$params['startdaterange']]);
+        if(isset($params['enddaterange'])) array_push($where,["DeclaratedDate",'<=',$params['enddaterange']]);
+        if(isset($params['itemid'])) {
+
+            $selection = array_filter($selection, function($v){
+                echo ($v);
+                return $v != 'employeehours.EmployeeID';
+            });
+            array_push($where,["employeehours.EmployeeID",'=',$params['itemid']]);
+        }
         if(isset($params['employeehoursid'])) array_push($where,["EmployeeHoursID",'=',$params['employeehoursid']]);
         if(isset($params['status'])) array_push($where,["HoursAccorded",'=',$params['status']]);
 
         //if no where clauses, select all employees
         if (!count($where)>0) array_push($where,["employeehours.EmployeeID",'>',0]);
-
-        //fetch and return the result
-        $result = $this->db->table('employeehours')->innerjoin('departmentmemberlist','EmployeeID')->where($where)->get();
+            //fetch and return the result
+        $result = $this->db->table('employeehours')->selection($selection)->innerjoin('departmentmemberlist','EmployeeID')->where($where)->get();
         return (array)$result;
     }
 
     public function put(array $body, array $params): array
     {
+        // check for itemid
+        if (! isset($params['itemid'])) throw new TeapotException('Hours can only be updated at individual endpoints');
+        //check manager and employee for authorisation
+        if ( ( (! isset($params['itemid']) ) OR ( $this->employee != $params [ 'itemid' ] ) ) AND ( !$this->manager) ) throw new NotAuthorizedException('Hours can only be viewed by a manager or the object employee');
+
+        //check if all required parameters are set
+        $requiredParamsArray = ["EmployeeHoursID", "HoursAccorded", "AccordedByManager"];
+        foreach ($requiredParamsArray as $param)
+        {
+            if (! isset($params[$param])) throw new BadRequestException("Parameter '$param' is required");
+        }
+
+
+
         // TODO: Implement put() method.
         return [];
     }
