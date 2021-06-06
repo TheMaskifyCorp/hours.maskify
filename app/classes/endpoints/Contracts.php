1237ac7827ba31d378144c8c3646692ba287e7a8
Icro-dev <svenmuste@gmail.com>
2021-06-06 21:43:47 +0200

    committing local, merging remote

--- app/classes/endpoints/Contracts.php
+++ app/classes/endpoints/Contracts.php
@@ -28,18 +28,23 @@ class Contracts implements ApiEndpointInterface
 
     public function get (array $body, array $params) :array
     {
-        $employeeid = $params['employeeid'];
         $currentDateTime = date('Y-m-d ');
-        $onlycurrent =  $params['onlycurrent'];
+        if(isset($params['employeeid']))$employeeid = $params['employeeid'];
+        if(isset($params['departmentid']))$employeeid = $params['departmentid'];
+        if(!isset($params['onlycurrent']))$params['onlycurrent']=true;
+        $onlycurrent = $params['onlycurrent'];
 
+        //check if employeeid and department id are both set
+        if((isset($params['employeeid'])) AND (isset($params['departmentid']))) throw new BadRequestException("Cannot filter on both single Employee and Department");
+        if ( ( ! $this->manager) AND ( $employeeid !=$this->employee ) ) throw new NotAuthorizedException("Can only be viewed by a manager or the object employee");
         //return only contracts from specified DepartmentID
         if(isset($params['departmentid']))
         {
             $result = (array)$this->db->table('departmentmemberlist')->innerjoin('contracts','EmployeeID')->where(['DepartmentID','=',$params['departmentid']])->get();
             return (array)$result;
         }
-        if((isset($params['employeeid'])) AND (isset($params['departmentid']))) throw new BadRequestException("Cannot filter on both single Employee and Department");
-        if ( ( ! $this->manager) AND ( $employeeid !=$this->employee ) ) throw new NotAuthorizedException("Can only be viewed by a manager or the object employee");
+
+
         if(isset($params['onlycurrent'])) {
             $currentDateTime = date('Y-m-d ');
             $where = [];
@@ -57,7 +62,7 @@ class Contracts implements ApiEndpointInterface
                 return (array)$result;
             }
             //return expired contracts
-            if ($onlycurrent == 'false') {
+            if ($onlycurrent == false) {
                 //add parameters to an array
                 array_push($where, ["contracts.ContractEndDate", '<=', $currentDateTime]);
 
@@ -69,13 +74,6 @@ class Contracts implements ApiEndpointInterface
 
                 return (array)$result;
             }
-        }
-
-        //Show only contracts for object-employee
-        if ( ( ! $this->manager) AND ( $employeeid !=$this->employee ) ) throw new NotAuthorizedException("Can only be viewed by a manager or the object employee");
-        $response = (array)$this->db->table('contracts')->where(['contracts.EmployeeID','=',$employeeid])->get();
-        if (isset( $response )) {
-            return (array)$response;
         }
     }
 
@@ -91,7 +89,7 @@ class Contracts implements ApiEndpointInterface
         if( ! $this->manager) throw new NotAuthorizedException("Contracts can only be created by a manager");
         //check of het op /contracts endpoint gebeurd
         if (isset($params['itemid'])) throw new BadRequestException('Contracts can only be created at top-level endpoint /contracts');
-        if((isset($body['ContractEndDate'])) AND $body['ContractEndDate'] < $body['ContractStartDate']) throw new BadRequestException("End date cannot be earlier than start date");
+        if((isset($body['ContractEndDate'])) AND strtotime($body['ContractEndDate']) < strtotime($body['ContractStartDate'])) throw new BadRequestException("End date cannot be earlier than start date");
         //verwachte variabelen
         $required = ["EmployeeID", "ContractStartDate", "WeeklyHours"];
         $optional = ['ContractEndDate','PayRate'];
@@ -140,7 +138,7 @@ class Contracts implements ApiEndpointInterface
                 throw new NotAuthorizedException('This request can only be performed by a manager');
 
             // check if both employeeid and startdate params are set
-            if ( isset( $params [ 'employeeid' ]  ) AND isset($params['contractstartdate'] )) {
+                if ( isset ($params [ 'employeeid' ] , $params['contractstartdate'])){
                 array_push($where, ["contracts.EmployeeID", '=', $employeeid]);
                 array_push($where, ["contracts.ContractStartDate", '=', $contractstartdate]);
             }
@@ -183,6 +181,25 @@ class Contracts implements ApiEndpointInterface
                         throw new NotFoundException( "employeeid not found");
                     break;
 
+                case "onlycurrent":
+                    if (is_bool($value))
+                        throw new BadRequestException("emplooyeeid cannot exceed 15 characters");
+
+                    break;
+
+                case "departmentid":
+                    if (strlen((string)$value) > 15)
+                        throw new BadRequestException("departmentid cannot exceed 15 characters");
+
+                    //parameter must be an integer
+                    if (!preg_match('/^[0-9]{0,15}$/', $value))
+                        throw new BadRequestException("departmentid must be an integer");
+
+                    //parameter must be existing department
+                    if (! $db->table('departmenttypes')->exists(['DepartmentID' => $value]))
+                        throw new NotFoundException( "departmentid not found");
+                    break;
+
                 case "contractstartdate":
                     if (strlen((string)$value) > 15)
                         throw new BadRequestException("contractstartdate cannot exceed 15 characters");
@@ -203,18 +220,13 @@ class Contracts implements ApiEndpointInterface
      */
     private function validatePostRequest(array $request)
     {
-        $requiredString =["EmployeeID"];
         $requiredDate=["ContractStartDate", "ContractEndDate"];
-        $requiredAlphaNum = ["WeeklyHours", "PayRate"];
+        $requiredAlphaNum = ["EmployeeID", "WeeklyHours", "PayRate"];
         if (isset($request['EmployeeID'])) if ( ! preg_match ( '/[0-9]{1,11}$/', $request['EmployeeID'] )) throw new BadRequestException("EmployeeID must be integer");
         if (isset($request['WeeklyHours'])) if ( ! preg_match ( '/[0-9]{1,11}$/', $request['WeeklyHours'] )) throw new BadRequestException("WeeklyHours must be integer");
         if (isset($request['PayRate'])) if ( ! preg_match ( '/[0-9]{1,11}$/', $request['PayRate'] )) throw new BadRequestException("PayRate must be integer");
         if (isset($request['ContractStartDate'])) if ( ! preg_match ( '/[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $request['ContractStartDate'])) throw new BadRequestException("ContractStartDate must be formatted as: YYYY-MM-DD");
         if (isset($request['ContractEndDate'])) if ( ! preg_match ( '/[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $request['ContractEndDate'])) throw new BadRequestException("ContractEndDate must be formatted as: YYYY-MM-DD");
-        foreach($requiredString as $key)
-        {
-            if (isset($request[$key])) if ( ! preg_match ( '/[A-z0-9]+$/', $request[$key]) ) throw new BadRequestException("$key can only contain alphanumeric symbols");
-        }
         foreach($requiredAlphaNum as $key)
         {
             if (isset($request[$key])) if ( ! preg_match ( '/[A-z0-9]+$/', $request[$key]) ) throw new BadRequestException("$key can only contain alphanumeric symbols");
@@ -225,5 +237,4 @@ class Contracts implements ApiEndpointInterface
         }
 
     }
-
 }
