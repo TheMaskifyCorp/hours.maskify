d4c56354142417dffa61579c6951ab27928bcbfc
Icro-dev <svenmuste@gmail.com>
2021-06-06 13:23:48 +0200

    everything ready except for delete

--- app/classes/endpoints/Contracts.php
+++ app/classes/endpoints/Contracts.php
@@ -38,11 +38,6 @@ class Contracts implements ApiEndpointInterface
             $result = (array)$this->db->table('departmentmemberlist')->innerjoin('contracts','EmployeeID')->where(['DepartmentID','=',$params['departmentid']])->get();
             return (array)$result;
         }
-        //return only the current contracts
-//        if(isset($params['onlycurrent']))
-//        {
-//            $onlycurrent = $params['onlycurrent'];
-//        } else $onlycurrent = true;
         if((isset($params['employeeid'])) AND (isset($params['departmentid']))) throw new BadRequestException("Cannot filter on both single Employee and Department");
         if ( ( ! $this->manager) AND ( $employeeid !=$this->employee ) ) throw new NotAuthorizedException("Can only be viewed by a manager or the object employee");
         if(isset($params['onlycurrent'])) {
@@ -94,8 +89,9 @@ class Contracts implements ApiEndpointInterface
     public function post(array $body, array $params) :array
     {
         if( ! $this->manager) throw new NotAuthorizedException("Contracts can only be created by a manager");
-        //check of het op /employees gebeurd
+        //check of het op /contracts endpoint gebeurd
         if (isset($params['itemid'])) throw new BadRequestException('Contracts can only be created at top-level endpoint /contracts');
+        if((isset($body['ContractEndDate'])) AND $body['ContractEndDate'] < $body['ContractStartDate']) throw new BadRequestException("End date cannot be earlier than start date");
         //verwachte variabelen
         $required = ["EmployeeID", "ContractStartDate", "WeeklyHours"];
         $optional = ['ContractEndDate','PayRate'];
@@ -118,57 +114,49 @@ class Contracts implements ApiEndpointInterface
         $contractCreated = $this->db->table('contracts')->insert($requestParams);
 
         // throw error if contract is not created
-        if ($contractCreated !== true) throw new BadRequestException( ["Could not create contract"] );
-    }
-    public function put (array $body, array $params) :array {
-        if (( ! $this->manager) AND (! ($this->employee == $params['itemid'] ) ) ) throw new NotAuthorizedException("Employees can only be updated by a manager, or the object employee");
-        if (!isset( $params['itemid']) ) throw new TeapotException("Employees can only be updated at employee-specific endpoints");
-        //validate body
-        $this->validatePostRequest($body);
-        $response = [];
-        $optional = ['DocumentNumberID','Email',"FirstName", "LastName", "PhoneNumber", "Street", "HouseNumber","City", "PostalCode", "DateOfBirth", "FunctionTypeID","DepartmentID"];
-        //check if departmentID must be altered, and if so do it
-        if(isset($body['DepartmentID'])) {
-            $dpResult = $this->db->table("departmentmembertypes")->update(['DepartmentID'], ['EmployeeID', "=", $params['itemid']]);
-            if ($dpResult !== true) throw new BadRequestException("Could not update department");
-            unset($body['DepartmentID']);
-        }
-        var_dump($params['itemid']);
-        $empResult = $this->db->table('employees')->update($body,['EmployeeID','=',$params['itemid']]);
-        if ($empResult !== true) throw new BadRequestException("Could not update Employee");
-        return ["Employee Updated Succesfully"];
-    }
-    public function delete (array $body, array $params) :array{
-        throw new TeapotException("Employees can not be deleted. Update current contract to alter end-date");
-        return [];
+        if ($contractCreated !== true) throw new BadRequestException( "Could not create contract" );
+        return ["New contract created"];
     }
 
-    //private functies ter ondersteuning vd public functies
-
-    /**
-     * @param int $itemID
-     * @return array
-     * @throws NotAuthorizedException
-     */
-    private function returnDepartmentEmployees(int $itemID): array
+    //Geen put in contracts
+    public function put (array $body, array $params) :array
     {
-        if (! $this->manager) throw new NotAuthorizedException("Can only be viewed by a manager");
-        return (array)$this->db->table('employees')->innerjoin('departmentmemberlist','EmployeeID')->where(['DepartmentID','=',$itemID])->get();
+        if(isset($body)) throw new BadRequestException("Can not update existing contracts, create a new contract instead");
     }
 
-    /**
-     * @param int $itemID
-     * @return array
-     * @throws NotAuthorizedException
-     */
-    private function returnSingleItem(int $employeeid): array
+
+    public function delete (array $body, array $params) :array
     {
-        if ( ( ! $this->manager) AND ( $employeeid !=$this->employee ) ) throw new NotAuthorizedException("Can only be viewed by a manager or the object employee");
-        $response = (array)$this->db->table('contracts')->where(['contracts.EmployeeID','=',$employeeid])->get();
-        if (isset( $response )) {
-            return (array)$response[0];
-        }
+        $employeeid = $params['employeeid'];
+        $contractstartdate = $params['contractstartdate'];
+        var_dump($contractstartdate);
+        $where = [];
+
+            //check for object id
+            if (! isset ( $params['employeeid'] ) OR (! isset($params['contractstartdate'] )))
+                throw new BadRequestException('emplopyeeid or contractstartdate is not set');
+
+            //check if user is manager
+            if ( !$this->manager)
+                throw new NotAuthorizedException('This request can only be performed by a manager');
+
+            // check if both employeeid and startdate params are set
+            if ( isset( $params [ 'employeeid' ]  ) AND isset($params['contractstartdate'] )) {
+                array_push($where, ["contracts.EmployeeID", '=', $employeeid]);
+                array_push($where, ["contracts.ContractStartDate", '=', $contractstartdate]);
+            }
+
+                //try database request
+                try {
+                    $this->db->table('contracts')->delete($where);
+                } catch (\Exception $e) {
+                    throw new BadRequestException('Error updating database');
+                }
+                //return message
+                return ["Contract with {$params['employeeid']} and {$params['contractstartdate']} deleted"];
+
     }
+
 
     /**
      * @throws BadRequestException
@@ -178,21 +166,24 @@ class Contracts implements ApiEndpointInterface
         $requiredString =["EmployeeID"];
         $requiredDate=["ContractStartDate", "ContractEndDate"];
         $requiredAlphaNum = ["WeeklyHours", "PayRate"];
-        if (isset($request['FunctionTypeID'])) if ( ! preg_match ( '/[0-9]{1,11}$/', $request['FunctionTypeID'] )) throw new BadRequestException("Functiontype must be integer");
-        if (isset($request['DepartmentID'])) if ( ! preg_match ( '/[0-9]{1,11}$/', $request['DepartmentID'] )) throw new BadRequestException("DepartmentID must be integer");
-        if (isset($request['PhoneNumber'])) if ( ! preg_match ( '/\+[0-9]{11}$/', $request['PhoneNumber']) ) throw new BadRequestException("PhoneNumber must be like +31612345678");
-        if (isset($request['DateOfBirth'])) if ( ! preg_match ( '/[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $request['DateOfBirth'])) throw new BadRequestException("DateOfBirth must be like YYYY-MM-DD");
+        if (isset($request['EmployeeID'])) if ( ! preg_match ( '/[0-9]{1,11}$/', $request['EmployeeID'] )) throw new BadRequestException("EmployeeID must be integer");
+        if (isset($request['WeeklyHours'])) if ( ! preg_match ( '/[0-9]{1,11}$/', $request['WeeklyHours'] )) throw new BadRequestException("WeeklyHours must be integer");
+        if (isset($request['PayRate'])) if ( ! preg_match ( '/[0-9]{1,11}$/', $request['PayRate'] )) throw new BadRequestException("PayRate must be integer");
+        if (isset($request['ContractStartDate'])) if ( ! preg_match ( '/[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $request['ContractStartDate'])) throw new BadRequestException("ContractStartDate must be formatted as: YYYY-MM-DD");
+        if (isset($request['ContractEndDate'])) if ( ! preg_match ( '/[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $request['ContractEndDate'])) throw new BadRequestException("ContractEndDate must be formatted as: YYYY-MM-DD");
         foreach($requiredString as $key)
         {
-            if (isset($request[$key])) if ( ! preg_match ( '/[A-z]+$/', $request[$key]) ) throw new BadRequestException("$key can only contain upper- and lowercase letters");
+            if (isset($request[$key])) if ( ! preg_match ( '/[A-z0-9]+$/', $request[$key]) ) throw new BadRequestException("$key can only contain alphanumeric symbols");
         }
         foreach($requiredAlphaNum as $key)
         {
             if (isset($request[$key])) if ( ! preg_match ( '/[A-z0-9]+$/', $request[$key]) ) throw new BadRequestException("$key can only contain alphanumeric symbols");
         }
-        $emailRegex = '/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD';
-        if (isset($request["Email"])) if (!preg_match( $emailRegex, $request['Email'])) throw new BadRequestException("Email adres is not a valid e-mail");
-        if (isset($request["Email"])) if ($this->db->table("employees")->where(["Email","=",$request['Email']])->count() > 0) throw new BadRequestException("Emailadres already in database");
+        foreach($requiredDate as $key)
+        {
+            if (isset($request[$key])) if ( ! preg_match ( '/[A-z0-9]+$/', $request[$key]) ) throw new BadRequestException("$key can only contain alphanumeric symbols");
+        }
+
     }
 
 }
