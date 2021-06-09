165726e5d3cb9888f3947a92b15b925ab7c55d61
Jeroen Rijkse <jeroen@rijkse>
2021-06-09 21:34:47 +0200

    updated init to work with installer

--- app/init.php
+++ app/init.php
@@ -1,26 +1,28 @@
 <?php
 session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] ."/vendor/autoload.php";
-
+$docRoot = $_SERVER['DOCUMENT_ROOT'];
 $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
 
 try {
     $dotenv->load();
+
+    $hash = new Hash;
+    $db = new Database();
+    $auth = new Auth($db, $hash);
+
+    $jwt = "";
+    if (isset($_SESSION['employee'])) {
+        $token = array(
+            'eid' => ($_SESSION['employee']),
+            'manager' => ($_SESSION['manager']),
+            'iat' => time()
+        );
+        $jwt = Firebase\JWT\JWT::encode($token, $_ENV['JWTSECRET']);
+        setcookie("jwt", $jwt);
+    }
+
 } catch (Exception $e) {
     if(!$_SERVER['REQUEST_URI'] == "/view/install/index.php")
        header("Location: /view/install/index.php");
 }
-$jwt = "";
-if (isset($_SESSION['employee'])) {
-    $token = array(
-        'eid' => ($_SESSION['employee']),
-        'manager' => ($_SESSION['manager']),
-        'iat' => time()
-    );
-    $jwt = Firebase\JWT\JWT::encode($token, $_ENV['JWTSECRET']);
-    setcookie("jwt", $jwt);
-}
-$hash = new Hash;
-$db = new Database();
-$auth = new Auth($db, $hash);
-$docRoot = $_SERVER['DOCUMENT_ROOT'];
