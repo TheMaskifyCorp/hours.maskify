28ae6aa7f4c415978e745097b4e4943346472d59
Jeroen Rijkse <jeroen@rijkse>
2021-06-13 16:45:18 +0200

    404-image uploader klaar

--- view/404.php
+++ view/404.php
@@ -5,6 +5,8 @@ require_once $_SERVER['DOCUMENT_ROOT']."/app/init.php";
  */
 require_once $docRoot.'/view/includes/header.php';
 ?>
+<h3>Sorry, the page could not be found.</h3>
+<h4>Perhaps you made a typo?</h4>
 <img src="/view/images/404.png" class='w-100' alt="notfound">
 <?php
 require_once $docRoot.'/view/includes/footer.php';
