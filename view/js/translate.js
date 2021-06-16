ad6b559d05633e89c9d11c30430b51d6f9e45394
Jeroen Rijkse <jeroen@rijkse>
2021-06-16 19:57:15 +0200

    minor changes

--- view/js/translate.js
+++ view/js/translate.js
@@ -1,12 +1,10 @@
  function Translate() {
 	//initialization
-	this.init =  function(attribute, lng){
-		this.attribute = attribute;
+	this.init =  function(lng){
 		this.lng = lng;
 	}
 	//translate
 	this.process = function(){
-		const _self = this;
 		axios.get('/view/includes/translations/'+this.lng+'.json')
 			.then(data => data.data)
 			.then(LngObject => {
@@ -14,7 +12,7 @@
 				let l = allDom.length
 				for(let i = 0; i < l-1; i++){
 					let elem = allDom[i];
-					let key = elem.getAttribute(_self.attribute);
+					let key = elem.getAttribute("data-lang");
 					if(key != null) {
 						elem.innerHTML = LngObject[key]  ;
 					}
@@ -23,6 +21,6 @@
 }
 function loadTranslation(lang) {
 	var translate = new Translate();
-	translate.init("data-lang", lang);
+	translate.init( lang);
 	translate.process();
 }
--- view/js/useSearch.js
+++ view/js/useSearch.js
@@ -6,4 +6,5 @@ faqForm.addEventListener('submit', (event) => {
     // get the form data
     const theData = new FormData(faqForm)
     UseSearchTerm( theData.get('faqSearch') );
+    faqForm.reset()
 })
